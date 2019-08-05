35d15ceae2cc4d2d2da352f3c888e1f0b7ae98d9
stkrizh <st.krizh@gmail.com>
2019-08-05 11:36:28 +0300

    Add storage unit tests

---
+++ scoring/tests/unit/test_store.py
@@ -0,0 +1,24 @@
+import unittest
+
+from mock import Mock, patch
+from redis import ConnectionError
+
+from scoring.store import Storage, StorageError
+
+
+class TestStorage(unittest.TestCase):
+    @patch("scoring.settings.RETRY_DELAY", 0.01)
+    def test_connection_error(self):
+        storage = Storage()
+        storage.redis_client = Mock(
+            **{
+                "get.side_effect": ConnectionError,
+                "set.side_effect": ConnectionError,
+            }
+        )
+
+        self.assertIs(None, storage.cache_set("a", 42, 60))
+        self.assertIs(None, storage.cache_get("a"))
+
+        with self.assertRaises(StorageError):
+            storage.get("a")

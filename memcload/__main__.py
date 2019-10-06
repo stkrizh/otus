982d2dc6bcb743eee4d31924b27393145187cbdc
stkrizh <st.krizh@gmail.com>
2019-10-06 23:29:49 +0300

    Add tests

--- memcload/__main__.py
+++ memcload/__main__.py
@@ -1,80 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-import gzip
-import sys
 import glob
+import gzip
 import logging
 import multiprocessing as mp
+import os
+import sys
+
 from collections import Counter
-from enum import Enum
 from functools import partial
 from optparse import OptionParser
-from typing import Dict, List, NamedTuple
+from typing import Dict
 
-# brew install protobuf
-# protoc  --python_out=. ./appsinstalled.proto
-# pip install protobuf
-from . import appsinstalled_pb2
-
-# pip install python-memcached
 import memcache
 
+from . import appsinstalled_pb2
+from .types import AppsInstalled, DeviceType, ProcessingStatus
+
+
 NORMAL_ERR_RATE = 0.01
-
-
-class DeviceType(Enum):
-    IDFA = "idfa"
-    GAID = "gaid"
-    ADID = "adid"
-    DVID = "dvid"
-
-
-class ProcessingStatus(Enum):
-    OK = 1
-    ERROR = -1
-    SKIP = 0
-
-
-class AppsInstalled(NamedTuple):
-    dev_type: DeviceType
-    dev_id: str
-    lat: float
-    lon: float
-    apps: List[int]
-
-    @classmethod
-    def from_raw(cls, line: str) -> "AppsInstalled":
-        line_parts = line.strip().split("\t")
-
-        if len(line_parts) < 5:
-            raise ValueError("Invalid format.")
-
-        raw_dev_type, dev_id, raw_lat, raw_lon, raw_apps = line_parts
-
-        try:
-            raw_dev_type = raw_dev_type.strip().lower()
-            dev_type = DeviceType(raw_dev_type)
-        except ValueError:
-            raise ValueError(f"Unknown device type: {raw_dev_type}")
-
-        if not dev_id:
-            raise ValueError("Device ID missed.")
-
-        try:
-            apps = [int(a.strip()) for a in raw_apps.split(",")]
-        except ValueError:
-            apps = [
-                int(a.strip()) for a in raw_apps.split(",") if a.isdigit()
-            ]
-            logging.info("Not all user apps are digits: `%s`" % line)
-
-        try:
-            lat, lon = float(raw_lat), float(raw_lon)
-        except ValueError:
-            logging.info("Invalid geo coords: `%s`" % line)
-
-        return cls(dev_type, dev_id, lat, lon, apps)
 
 
 def dot_rename(path):
@@ -92,8 +34,6 @@ def insert_appsinstalled(
     key = "%s:%s" % (appsinstalled.dev_type.value, appsinstalled.dev_id)
     ua.apps.extend(appsinstalled.apps)
     packed = ua.SerializeToString()
-    # @TODO persistent connection
-    # @TODO retry and timeouts!
     try:
         if dry_run:
             logging.debug(
@@ -176,29 +116,9 @@ def main(options):
         dot_rename(fn)
 
 
-def prototest():
-    sample = (
-        "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\n"
-        "gaid\t7rfw452y52g2gq4g\t55.55\t42.42\t7423,424"
-    )
-    for line in sample.splitlines():
-        dev_type, dev_id, lat, lon, raw_apps = line.strip().split("\t")
-        apps = [int(a) for a in raw_apps.split(",") if a.isdigit()]
-        lat, lon = float(lat), float(lon)
-        ua = appsinstalled_pb2.UserApps()
-        ua.lat = lat
-        ua.lon = lon
-        ua.apps.extend(apps)
-        packed = ua.SerializeToString()
-        unpacked = appsinstalled_pb2.UserApps()
-        unpacked.ParseFromString(packed)
-        assert ua == unpacked
-
-
 if __name__ == "__main__":
     op = OptionParser()
 
-    op.add_option("-t", "--test", action="store_true", default=False)
     op.add_option("-l", "--log", action="store", default=None)
     op.add_option("--dry", action="store_true", default=False)
     op.add_option(
@@ -217,10 +137,6 @@ if __name__ == "__main__":
         format="[%(asctime)s] %(levelname).1s %(message)s",
         datefmt="%Y.%m.%d %H:%M:%S",
     )
-
-    if opts.test:
-        prototest()
-        sys.exit(0)
 
     logging.info("Memc loader started with options: %s" % opts)
 
---
+++ memcload/types.py
@@ -0,0 +1,61 @@
+import logging
+
+from enum import Enum
+from typing import List, NamedTuple
+
+
+class DeviceType(Enum):
+    IDFA = "idfa"
+    GAID = "gaid"
+    ADID = "adid"
+    DVID = "dvid"
+
+
+class ProcessingStatus(Enum):
+    OK = 1
+    ERROR = -1
+    SKIP = 0
+
+
+class AppsInstalled(NamedTuple):
+    dev_type: DeviceType
+    dev_id: str
+    lat: float
+    lon: float
+    apps: List[int]
+
+    @classmethod
+    def from_raw(cls, line: str) -> "AppsInstalled":
+        line_parts = line.strip().split("\t")
+
+        if len(line_parts) < 5:
+            raise ValueError("Invalid format.")
+
+        raw_dev_type, dev_id, raw_lat, raw_lon, raw_apps = line_parts
+
+        try:
+            raw_dev_type = raw_dev_type.strip().lower()
+            dev_type = DeviceType(raw_dev_type)
+        except ValueError:
+            raise ValueError(f"Unknown device type: {raw_dev_type}")
+
+        if not dev_id:
+            raise ValueError("Device ID missed.")
+
+        try:
+            apps = [int(a.strip()) for a in raw_apps.split(",")]
+        except ValueError:
+            apps = [
+                int(a.strip())
+                for a in raw_apps.split(",")
+                if a.strip().isdigit()
+            ]
+            logging.info("Not all user apps are digits: `%s`" % line)
+
+        try:
+            lat, lon = float(raw_lat), float(raw_lon)
+        except ValueError:
+            lat = lon = float("nan")
+            logging.info("Invalid geo coords: `%s`" % line)
+
+        return cls(dev_type, dev_id, lat, lon, apps)

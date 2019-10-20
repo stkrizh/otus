08da305ed7eea3a04e41eb7980e3b3962296dbeb
stkrizh <st.krizh@gmail.com>
2019-10-20 14:54:23 +0300

    Black format

--- protobuf_serializer/setup.py
+++ protobuf_serializer/setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, Extension
 
-module1 = Extension("pb",
-                    sources=["pb.c", "deviceapps.pb-c.c"],
-                    extra_compile_args=["-g"],
-                    libraries=["protobuf-c"],
-                    library_dirs=["/usr/lib"],
-                    include_dirs=["/usr/include/google/protobuf-c/"],
-                    )
+module1 = Extension(
+    "pb",
+    sources=["pb.c", "deviceapps.pb-c.c"],
+    extra_compile_args=["-g"],
+    libraries=["protobuf-c"],
+    library_dirs=["/usr/lib"],
+    include_dirs=["/usr/include/google/protobuf-c/"],
+)
 
-setup(name="pb",
-      version="1.0",
-      description="Protobuf (de)serializer",
-      test_suite="tests",
-      ext_modules=[module1])
+setup(
+    name="pb",
+    version="1.0",
+    description="Protobuf (de)serializer",
+    test_suite="tests",
+    ext_modules=[module1],
+)
